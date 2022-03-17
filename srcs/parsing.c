37b2f0ae8cf7e0d7eed31f67273d4cc89831d5b4
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-17 16:04:02 +0100

    changed file opens to error correctly

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,12 +6,28 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:10:54 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/16 14:10:23 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/17 15:58:35 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
+
+static void	open_files(char **argv, t_input *input)
+{
+	input->fd[0] = open(argv[1], O_RDONLY);
+	if (input->fd[0] < 0)
+		perror(argv[1]);
+	input->fd[1] = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (input->fd[1] < 0)
+		perror(argv[4]);
+	if (input->fd[0] < 0 || input->fd[1] < 0)
+		exit(errno);
+}
+
 
 t_input	parse_input(int argc, char **argv, char **envp)
 {
@@ -19,16 +35,11 @@ t_input	parse_input(int argc, char **argv, char **envp)
 	int		i;
 
 	i = 0;
-	if (argc != 5)
-		error_exit(INVALID_ARGC);
-	ret.f1 = open(argv[1], O_RDONLY);
-	ret.f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC | 0644);
-	if (ret.f1 < 0 || ret.f2 < 0)
-		error_exit(INVALID_FILES);
 	ret.cmd1 = argv[2];
 	ret.cmd2 = argv[3];
 	ret.cmd1_args = ft_split(argv[2], ' ');
 	ret.cmd2_args = ft_split(argv[3], ' ');
+	open_files(argv, &ret);
 	while (envp[i])
 	{
 		if (!ft_strncmp(envp[i], "PATH=", 5))
