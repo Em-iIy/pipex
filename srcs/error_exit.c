2f19dafe49eeddbd296347145ff42d56fbfbdeb7
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-21 18:10:34 +0100

    removed exiting with errno

--- srcs/error_exit.c
+++ srcs/error_exit.c
@@ -6,16 +6,15 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:28:47 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/17 13:30:39 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/21 18:08:33 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
 
 void	error_exit(const char *msg)
 {
 	perror(msg);
-	exit(errno);
+	exit(EXIT_FAILURE);
 }
