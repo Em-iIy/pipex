b0741aa9c2c35d6abc14c7008dd1d61e7203edbb
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-21 18:12:15 +0100

    made the main pipex function

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,27 +6,48 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:23:56 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/17 15:55:07 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/21 17:37:55 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
+#include <unistd.h>
 
-// static void	pipex();
-
-static void	print_input(t_input input)
+static void	pipex(t_input input, int pipe_fd[2], char **envp)
 {
-	int		i;
+	pid_t	child1;
+	pid_t	child2;
+	int		status;
 
-	printf("INPUT\ncmd1:\t\t\"%s\"\ncmd2:\t\t\"%s\"\n", input.cmd1, input.cmd2);
-	for (i = 0; input.path[i]; i++)
-		printf("PATH:\t\t\"%s\"\n", input.path[i]);
-	for (i = 0; input.cmd1_args[i]; i++)
-		printf("cmd1 args:\t\"%s\"\n", input.cmd1_args[i]);
-	for (i = 0; input.cmd2_args[i]; i++)
-		printf("cmd2 args:\t\"%s\"\n", input.cmd2_args[i]);
+	child1 = fork();
+	if (child1 < 0)
+		error_exit("Fork");
+	else if (child1 == 0)
+		child_one(input, pipe_fd, envp);
+	child2 = fork();
+	if (child2 < 0)
+		error_exit("Fork");
+	else if (child2 == 0)
+		child_two(input, pipe_fd, envp);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	waitpid(child1, &status, 0);
+	waitpid(child2, &status, 0);
+	if (WIFEXITED(status))
+		exit(WEXITSTATUS(status));
 }
+
+// static void	print_input(t_input input)
+// {
+// 	int		i;
+
+// 	printf("INPUT\ncmd1:\t\t\"%s\"\ncmd2:\t\t\"%s\"\n", input.cmd1, input.cmd2);
+// 	for (i = 0; input.cmd1_args[i]; i++)
+// 		printf("cmd1 args:\t\"%s\"\n", input.cmd1_args[i]);
+// 	for (i = 0; input.cmd2_args[i]; i++)
+// 		printf("cmd2 args:\t\"%s\"\n", input.cmd2_args[i]);
+// }
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -36,10 +57,10 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 5)
 	{
 		write(2, "Error, invalid amount of arguments\n", 35);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	input = parse_input(argc, argv, envp);
 	if (pipe(pipe_fd) < 0)
 		error_exit("pipe");
-	print_input(input);
+	pipex(input, pipe_fd, envp);
 }
