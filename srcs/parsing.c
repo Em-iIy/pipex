dc3122ecc8f71bda1a3fc30da8650fb9e8547d63
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-21 18:11:57 +0100

    added function to return executable path

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 12:10:54 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/17 15:58:35 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/21 18:09:02 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <errno.h>
 
 static void	open_files(char **argv, t_input *input)
 {
@@ -25,26 +24,47 @@ static void	open_files(char **argv, t_input *input)
 	if (input->fd[1] < 0)
 		perror(argv[4]);
 	if (input->fd[0] < 0 || input->fd[1] < 0)
-		exit(errno);
+		exit(EXIT_FAILURE);
 }
 
+static char	*check_cmd(char	*cmd, char	**path)
+{
+	char	*ret;
+	char	*temp;
+	int		i;
+
+	i = 0;
+	while (path[i])
+	{
+		temp = ft_strjoin(path[i], "/");
+		ret = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(ret, X_OK) == 0)
+			return (ret);
+		free(ret);
+		i++;
+	}
+	return (NULL);
+}
 
 t_input	parse_input(int argc, char **argv, char **envp)
 {
 	t_input	ret;
+	char	**path;
 	int		i;
 
 	i = 0;
-	ret.cmd1 = argv[2];
-	ret.cmd2 = argv[3];
 	ret.cmd1_args = ft_split(argv[2], ' ');
 	ret.cmd2_args = ft_split(argv[3], ' ');
 	open_files(argv, &ret);
 	while (envp[i])
 	{
 		if (!ft_strncmp(envp[i], "PATH=", 5))
-			ret.path = ft_split(&envp[i][5], ':');
+			path = ft_split(&envp[i][5], ':');
 		i++;
 	}
+	ret.cmd1 = check_cmd(ret.cmd1_args[0], path);
+	ret.cmd2 = check_cmd(ret.cmd2_args[0], path);
+	free_all(path);
 	return (ret);
 }
